d44ed05fe5b6696e4b2d8dbc615ddc6632f973e7
Ellis Thomas <103071503+EllisThomas0@users.noreply.github.com>
2026-03-03 08:42:43 -0500

    Update calculateDistances.js, uri literal

--- calculateDistances.js
+++ calculateDistances.js
@@ -2,7 +2,8 @@
 const MongoClient = require('mongodb').MongoClient;
 
 // Connection URL
-const uri = 'mongodb+srv://ethomas2:jKFf8pYKYZerXCVD@etbuscluster.fj6tntd.mongodb.net/ThesisData?retryWrites=true&w=majority';
+//Used a literal
+const uri = uriLiteral;
 
 // Database Name
 const dbName = 'ThesisData';
@@ -18,42 +19,6 @@ async function run() {
         const database = client.db(dbName);
         const busStops = database.collection(collectionName);
 
-        // // var filter = { 'FID' : 1 };
-        // // BusStop.findOne(filter)
-	    // // .then((b) => {
-        // //     console.log(b);
-        // //     res.json({'busStop' : b})
-	    // // 	// then show the form from the EJS template
-	    // // 	// res.render('eventform', {'person' : p})
-	    // // })
-        // // .catch((err) => {
-        // //     // res.type('html').status(200);
-        // //     // console.log('uh oh: ' + err);
-        // //     // res.send(err);
-        // //     console.log(err);
-        // // })
-        
-
-        // // Query for a movie that has the title 'Back to the Future'
-        // // const query = { title: 'Back to the Future' };
-        // var filter = { 'FID' : 1 };
-        // // const movie = await movies.findOne(query);
-        // const bStop = await busStops.findOne(filter);
-
-
-        // busStops.find({}).toArray((err, docs) => {
-        //     // if (err) {
-        //     //     console.error('Error occurred while fetching documents:', err);
-        //     //     client.close();
-        //     //     return;
-        //     // }
-            
-        //     // Loop through each document and print the value of the field named "Longitude"
-        //     // docs.forEach(doc => {
-        //     //     console.log('Longitude:', doc.Lon);
-                
-        //     // });
-        // });
         database.collection(collectionName).find({}).toArray(function(err, result) {
             if (err) throw err;
             console.log(result);
